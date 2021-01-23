85dcf2c1f7938cf1b6375cfb31b4e8ab4e4ebfeb
Stephen Lazaro <stephen.lazaro@gmail.com>
2021-01-22 19:04:43 -0800

    Start on Wedge tests

--- src/test/scala/mushi/WedgeSpec.scala
+++ src/test/scala/mushi/WedgeSpec.scala
@@ -8,18 +8,19 @@ import org.typelevel.discipline.Laws
 import org.typelevel.discipline.scalatest.FlatSpecDiscipline
 
 class WedgeSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with FlatSpecDiscipline {
-//  implicit def arbWedge[A: Arbitrary, B: Arbitrary]: Arbitrary[Wedge[A, B]] =
-//    Arbitrary(
-//      for {
-//        a <- Gen option Arbitrary.arbitrary[A]
-//        b <- if (a.isDefined) Gen some Arbitrary.arbitrary[B] else Gen const None
-//      } yield
-//        if (a.isDefined) Wedge.Present(a.get, b.get)
-//        else Wedge.Unaccounted
-//    )
+  implicit def arbWedge[A: Arbitrary, B: Arbitrary]: Arbitrary[Wedge[A, B]] =
+    Arbitrary(
+      for {
+        a <- Gen option Arbitrary.arbitrary[A]
+        b <- if (a.isEmpty) Gen option Arbitrary.arbitrary[B] else Gen const None
+     } yield
+       if (a.isDefined) Wedge.WLeft(a.get)
+       else if (b.isDefined) Wedge.WRight(b.get)
+       else Wedge.WEmpty
+    )
 //  checkAll("Wedge.FunctorLaws", FunctorTests[Wedge[Int, *]].functor[Int, Int, String])
 //  checkAll("Wedge.MonadLaws", MonadTests[Wedge[Int, *]].monad[Int, Int, String])
-//  checkAll("Wedge.BifunctorLaws", BifunctorTests[Wedge].bifunctor[Int, Int, Int, String, String, String])
+  checkAll("Wedge.BifunctorLaws", BifunctorTests[Wedge].bifunctor[Int, Int, Int, String, String, String])
 //  checkAll("Wedge.BifoldableLaws", BifoldableTests[Wedge].bifoldable[Int, String, Long])
 //  checkAll("Wedge.BitraverseLaws", BitraverseTests[Wedge].bitraverse[Option, Int, String, Long, Int, String, Long])
 }
