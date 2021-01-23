e93b6fb10098eba9d24f543f83a842b68174d3f9
Stephen Lazaro <stephen.lazaro@gmail.com>
2021-01-22 19:36:52 -0800

    Add Wedge Monad

--- src/main/scala/mushi/Wedge.scala
+++ src/main/scala/mushi/Wedge.scala
@@ -1,6 +1,6 @@
 package mushi
 
-import cats.{Bifunctor, Eq, Order}
+import cats.{Bifunctor, Eq, Functor, Monad, Order, Semigroup}
 import cats.instances.either._
 import cats.syntax.bifunctor._
 
@@ -18,15 +18,40 @@ object Wedge {
    * i.e. give us the representation in terms of Option
    */
   def reify[A, B](self: Wedge[A, B]): Option[Either[A, B]] = self match {
-    case WLeft(l) => Some(Left(l))
+    case WLeft(l)  => Some(Left(l))
     case WRight(r) => Some(Right(r))
-    case WEmpty => None
+    case WEmpty    => None
   }
 
   implicit val bifunctor: Bifunctor[Wedge] = new Bifunctor[Wedge] {
     def bimap[A, B, C, D](fac: Wedge[A, B])(f: A => C, g: B => D): Wedge[C, D] =
       Wedge(reify(fac).map(_.bimap(f, g)))
   }
+
+  implicit def functor[A]: Functor[Wedge[A, *]] = new Functor[Wedge[A, *]] {
+    def map[C, B](fac: Wedge[A, C])(f: C => B): Wedge[A, B] = fac match {
+      case WLeft(a)  => WLeft(a)
+      case WRight(c) => WRight(f(c))
+      case WEmpty    => WEmpty
+    }
+  }
+
+  implicit def monad[A: Semigroup]: Monad[Wedge[A, *]] = new Monad[Wedge[A, *]] {
+    def pure[C](c: C): Wedge[A, C] = WRight(c)
+    def flatMap[C, B](fac: Wedge[A, C])(f: C => Wedge[A, B]): Wedge[A, B] = fac match {
+      case WLeft(a)  => WLeft(a)
+      case WRight(c) => f(c)
+      case WEmpty    => WEmpty
+    }
+
+    def tailRecM[C, B](c: C)(f: C => Wedge[A, Either[C, B]]): Wedge[A, B] = f(c) match {
+      case WRight(Right(b)) => WRight(b)
+      case WRight(Left(c)) => tailRecM(c)(f)
+      case WLeft(a) => WLeft(a)
+      case WEmpty => WEmpty
+    }
+  }
+
 
   implicit def eq[A: Eq, B: Eq]: Eq[Wedge[A, B]] = Eq.by(reify)
   implicit def ord[A: Order, B: Order]: Order[Wedge[A, B]] = Order.by(reify)
--- src/test/scala/mushi/WedgeSpec.scala
+++ src/test/scala/mushi/WedgeSpec.scala
@@ -18,8 +18,8 @@ class WedgeSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with Fla
        else if (b.isDefined) Wedge.WRight(b.get)
        else Wedge.WEmpty
     )
-//  checkAll("Wedge.FunctorLaws", FunctorTests[Wedge[Int, *]].functor[Int, Int, String])
-//  checkAll("Wedge.MonadLaws", MonadTests[Wedge[Int, *]].monad[Int, Int, String])
+  checkAll("Wedge.FunctorLaws", FunctorTests[Wedge[Int, *]].functor[Int, Int, String])
+  checkAll("Wedge.MonadLaws", MonadTests[Wedge[Int, *]].monad[Int, Int, String])
   checkAll("Wedge.BifunctorLaws", BifunctorTests[Wedge].bifunctor[Int, Int, Int, String, String, String])
 //  checkAll("Wedge.BifoldableLaws", BifoldableTests[Wedge].bifoldable[Int, String, Long])
 //  checkAll("Wedge.BitraverseLaws", BitraverseTests[Wedge].bitraverse[Option, Int, String, Long, Int, String, Long])
